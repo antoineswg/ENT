dce1732d4ca5101d8167c42d0d41dca56d57dcdd
antoine <153529541+antoineswg@users.noreply.github.com>
2024-12-03 15:06:34 +0100

    ajout des infos utilisateurs en session

--- traitelogin.php
+++ traitelogin.php
@@ -14,9 +14,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if ($user) {
         if (password_verify($password, $user['password'])){
             $_SESSION['user_id'] = $user['id'];
-        $_SESSION['user_name'] = $user['name'];
-        header('Location: index.php?status=logged');
-        exit();
+            $_SESSION['user_name'] = $user['name'];
+            $_SESSION['user_surname'] = $user['surname'];
+            $_SESSION['user_mail'] = $user['mail'];
+            $_SESSION['user_password'] = $password;
+            header('Location: index.php?status=logged');
+            exit();
         }
         else {
             header('Location: index.php?erreur=password');
